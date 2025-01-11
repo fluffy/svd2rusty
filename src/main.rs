46db3210b548cce96ef1f2e7df69e896ab73cb7a
Cullen Jennings <fluffy@iii.ca>
2025-01-11 16:10:23 -0700

    start gen of stm32f0x7 files

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn parse_xml_with_xml_rs(file_path: &str) -> Result<(), Box<dyn std::error::Erro
     let mut wanted_peripheral = false;
     let mut inside_register = false;
     let mut inside_field = false;
+    let mut inside_enumerated_value = false;
     let mut inside_name = false;
     let mut inside_bit_offset = false;
 
@@ -30,6 +31,10 @@ fn parse_xml_with_xml_rs(file_path: &str) -> Result<(), Box<dyn std::error::Erro
                 }
                 if name.local_name == "field" {
                     inside_field = true;
+                    inside_enumerated_value= false;
+                }
+                if name.local_name == "enumeratedValue" {
+                    inside_enumerated_value= true;
                 }
                 if name.local_name == "name" {
                     inside_name = true;
@@ -60,6 +65,10 @@ fn parse_xml_with_xml_rs(file_path: &str) -> Result<(), Box<dyn std::error::Erro
                 if name.local_name == "field" {
                     inside_field = false;
                 }
+                if name.local_name == "enumeratedValue" {
+
+                    //inside_enumerated_value= false;
+                }
                 if name.local_name == "name" {
                     inside_name = false;
                 }
@@ -78,14 +87,14 @@ fn parse_xml_with_xml_rs(file_path: &str) -> Result<(), Box<dyn std::error::Erro
                         if content == "RCC" {
                             wanted_peripheral = true;
                         }
-                        if content == "USART6" {
+                        if content == "USART1" {
                             wanted_peripheral = true;
                         }
                         if content == "TIM1" {
-                            wanted_peripheral = true;
+                            wanted_peripheral = false;
                         }
                         if content == "TIM2" {
-                            wanted_peripheral = true;
+                            wanted_peripheral = false;
                         }
                     } else {
                         println!("//FOUND Peripheral: {}", content);
@@ -93,7 +102,7 @@ fn parse_xml_with_xml_rs(file_path: &str) -> Result<(), Box<dyn std::error::Erro
 
                     if wanted_peripheral {
                         let mut name = content.clone();
-                        if name == "USART6" {
+                        if name == "USART1" {
                             name = "USART".to_string();
                         }
                         if name == "TIM1" {
@@ -119,10 +128,18 @@ fn parse_xml_with_xml_rs(file_path: &str) -> Result<(), Box<dyn std::error::Erro
                     && inside_register
                     && inside_field
                     && inside_name
+                    && !inside_enumerated_value
                 {
-                    //println!("FOUND Field: {}", content);
-                    println!("        pub const {} : u32 ", content);
+                    if content != "Enabled"
+                        && content != "ENABLED"
+                        && content != "CLEAR"
+                        && content != "SET"
+                        && content != "CLEARED" {
+                        //println!("FOUND Field: {}", content);
+                        println!("        pub const {} : u32 ", content);
+                    }
                 }
+
                 if inside_peripheral
                     && wanted_peripheral
                     && inside_register
@@ -131,6 +148,7 @@ fn parse_xml_with_xml_rs(file_path: &str) -> Result<(), Box<dyn std::error::Erro
                 {
                     //println!("FOUND Offset: {}", content);
                     println!("            = {};", content);
+                    //inside_field = false;
                 }
                 //println!("Text: {}", content);
             }
